f06cf0f94fb9ec1bd37e35f10fc28392fb194099
nmhillusion <nguyenminhhieu.geek@gmail.com>
2022-06-01 07:16:18 +0700

    fixed config

--- test/renderer/main.test.ts
+++ test/renderer/main.test.ts
@@ -2,11 +2,21 @@ import * as fs from "fs";
 import { N2EngineRenderer } from "../../src/renderer/main";
 
 test("test renderer", function () {
+  const rootDir = __dirname + "/../../sample";
+  const outDir = __dirname + "/../../sampleDist";
+
+  if (!fs.existsSync(rootDir)) {
+    fs.mkdirSync(rootDir, { recursive: true });
+  }
+  if (!fs.existsSync(outDir)) {
+    fs.mkdirSync(outDir, { recursive: true });
+  }
+
   expect(() =>
     new N2EngineRenderer()
       .config({
-        rootDir: fs.realpathSync(__dirname + "/../../sample"),
-        outDir: fs.realpathSync(__dirname + "/../../sampleDist"),
+        rootDir: fs.realpathSync(rootDir),
+        outDir: fs.realpathSync(outDir),
         pug: {
           enabled: true,
         },
