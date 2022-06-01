11ddc75481d3c9585c10e33446a1d0449b00c583
nmhillusion <nguyenminhhieu.geek@gmail.com>
2022-06-02 05:09:42 +0700

    change command to tag-version

--- support/tag-version.ts
+++ support/tag-version.ts
@@ -1,6 +1,6 @@
 import * as shx from "shelljs";
 import { version } from "../package.json";
 
-const command = `git tag v${version} && git push -u`;
+const command = `git tag v${version}`;
 
 console.log(shx.exec(command));
