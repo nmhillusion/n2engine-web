69c1a18c0799fc7c13a2129d01997128e7779b70
nmhillusion <nguyenminhhieu.geek@gmail.com>
2022-06-02 05:19:09 +0700

    update github actions

--- support/tag-version.ts
+++ support/tag-version.ts
@@ -1,6 +1,6 @@
 import * as shx from "shelljs";
 import { version } from "../package.json";
 
-const command = `git tag v${version}`;
+const command = `git tag v${version} && git push --tag`;
 
 console.log(shx.exec(command));
