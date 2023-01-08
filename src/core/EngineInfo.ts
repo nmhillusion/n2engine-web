af8abaef9da5c75ebd5e804cdbbf2363c6213600
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-01-08 13:00:36 +0700

    update project name

--- src/core/EngineInfo.ts
+++ src/core/EngineInfo.ts
@@ -1,6 +1,6 @@
 
   export const EngineInfo = {
-    name: "n2ngin-bull-engine",
+    name: "@nmhillusion/n2ngin-bull-engine",
     version: "2023.0108.2",
     description: "n2engine series: BullEngine to render (pug|scss|typescript|assets|variables) to (html|css|javascript|assets)"
   };
