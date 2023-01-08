9b8295094b1ef935c35dcbfddf21bf58baa318e7
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-01-08 13:52:53 +0700

    fixed publish folder

--- src/core/EngineInfo.ts
+++ src/core/EngineInfo.ts
@@ -1,6 +1,6 @@
 
   export const EngineInfo = {
     name: "@nmhillusion/n2ngin-bull-engine",
-    version: "2023.0108.4",
+    version: "2023.0108.5",
     description: "n2engine series: BullEngine to render (pug|scss|typescript|assets|variables) to (html|css|javascript|assets)"
   };
