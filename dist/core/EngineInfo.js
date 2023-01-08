48dff949134d080ab0e26223403c3b656e7e010b
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-01-08 12:23:29 +0700

    increase version

--- dist/core/EngineInfo.js
+++ dist/core/EngineInfo.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.EngineInfo = void 0;
 exports.EngineInfo = {
     name: "n2ngin-bull-engine",
-    version: "2022.2.2+15",
+    version: "2023.1.8.1",
     description: "n2engine series: BullEngine to render (pug|scss|typescript|assets|variables) to (html|css|javascript|assets)"
 };
 //# sourceMappingURL=EngineInfo.js.map
--- src/core/EngineInfo.ts
+++ src/core/EngineInfo.ts
@@ -1,6 +1,6 @@
 
   export const EngineInfo = {
     name: "n2ngin-bull-engine",
-    version: "2022.2.2+15",
+    version: "2023.1.8.1",
     description: "n2engine series: BullEngine to render (pug|scss|typescript|assets|variables) to (html|css|javascript|assets)"
   };
