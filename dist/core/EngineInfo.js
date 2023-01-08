fcdb6fec621bd031ff644ec9322a4d316b1c0e71
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-01-08 13:10:11 +0700

    change command of ci

--- dist/core/EngineInfo.js
+++ dist/core/EngineInfo.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.EngineInfo = void 0;
 exports.EngineInfo = {
     name: "@nmhillusion/n2ngin-bull-engine",
-    version: "2023.0108.2",
+    version: "2023.0108.3",
     description: "n2engine series: BullEngine to render (pug|scss|typescript|assets|variables) to (html|css|javascript|assets)"
 };
 //# sourceMappingURL=EngineInfo.js.map
--- src/core/EngineInfo.ts
+++ src/core/EngineInfo.ts
@@ -1,6 +1,6 @@
 
   export const EngineInfo = {
     name: "@nmhillusion/n2ngin-bull-engine",
-    version: "2023.0108.2",
+    version: "2023.0108.3",
     description: "n2engine series: BullEngine to render (pug|scss|typescript|assets|variables) to (html|css|javascript|assets)"
   };
