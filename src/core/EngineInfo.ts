0f4352bb03ed0659149d369657745251a1ab4bc5
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-01-08 14:12:29 +0700

    missing package.json

--- src/core/EngineInfo.ts
+++ src/core/EngineInfo.ts
@@ -1,6 +1,6 @@
 
   export const EngineInfo = {
     name: "@nmhillusion/n2ngin-bull-engine",
-    version: "2023.0108.6",
+    version: "2023.0108.7",
     description: "n2engine series: BullEngine to render (pug|scss|typescript|assets|variables) to (html|css|javascript|assets)"
   };
