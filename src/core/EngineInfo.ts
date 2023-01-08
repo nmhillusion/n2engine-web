0236052e26408d25a1e82ab6d40f2c20f298acd2
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-01-08 12:47:55 +0700

    upgrade log version

--- src/core/EngineInfo.ts
+++ src/core/EngineInfo.ts
@@ -1,6 +1,6 @@
 
   export const EngineInfo = {
     name: "n2ngin-bull-engine",
-    version: "2023.1.8.1",
+    version: "2023.0108.1",
     description: "n2engine series: BullEngine to render (pug|scss|typescript|assets|variables) to (html|css|javascript|assets)"
   };
