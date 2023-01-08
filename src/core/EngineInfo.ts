7d773412aa66bbcc26415f3222a437214c085598
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-01-08 14:31:04 +0700

    fixed ci commands

--- src/core/EngineInfo.ts
+++ src/core/EngineInfo.ts
@@ -1,6 +1,6 @@
 
   export const EngineInfo = {
     name: "@nmhillusion/n2ngin-bull-engine",
-    version: "2023.0108.8",
+    version: "2023.0108.9",
     description: "n2engine series: BullEngine to render (pug|scss|typescript|assets|variables) to (html|css|javascript|assets)"
   };
