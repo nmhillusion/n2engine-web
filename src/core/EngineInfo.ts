c5232eacab6b169cbc4147a4b0ff1c73d23a8f9a
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-03-19 17:34:58 +0700

    fixed for location of user base ts config

--- src/core/EngineInfo.ts
+++ src/core/EngineInfo.ts
@@ -1,6 +1,6 @@
 
   export const EngineInfo = {
     name: "@nmhillusion/n2ngin-bull-engine",
-    version: "2023.319.1",
+    version: "2023.319.2",
     description: "n2engine series: BullEngine to render (pug|scss|typescript|assets|variables) to (html|css|javascript|assets)"
   };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 
-export const WORKSPACE_DIR: string = fs.realpathSync(process.cwd());
+export const WORKSPACE_DIR: string = fs.realpathSync(__dirname);
 
 console.log("WORKSPACE_DIR: ", WORKSPACE_DIR);
 
