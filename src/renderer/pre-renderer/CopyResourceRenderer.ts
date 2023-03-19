d1eb0ea19e83c39b8927adfe15d1b9d59adf0734
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-03-19 17:16:47 +0700

    fixed for copy resource default

--- src/renderer/pre-renderer/CopyResourceRenderer.ts
+++ src/renderer/pre-renderer/CopyResourceRenderer.ts
@@ -9,19 +9,11 @@ export class CopyResourceRenderer extends Renderable {
     outDir: string,
     renderConfig: RenderConfig
   ) {
-    if (
-      (
-        renderConfig.copyResource?.config?.extsToCopy || [
-          ".jpg",
-          ".jpeg",
-          ".png",
-          ".gif",
-          ".ico",
-          ".woff",
-          ".ttf",
-        ]
-      ).some((ext) => filePath.endsWith(ext))
-    ) {
+    let extsToCopy = [".jpg", ".jpeg", ".png", ".gif", ".ico", ".woff", ".ttf"];
+    if (Array.isArray(renderConfig.copyResource?.config?.extsToCopy)) {
+      extsToCopy = renderConfig.copyResource?.config?.extsToCopy;
+    }
+    if (extsToCopy.some((ext) => filePath.endsWith(ext))) {
       this.logger.info("copy resource for ", filePath);
       FileSystemHelper.copyFile({
         sourceFilePath: filePath,
